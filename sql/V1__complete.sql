b230f9d5685ce713caf6e9fd6670d1a1e7acbae0
chabare <chabare95@gmail.com>
2018-08-25 18:36:56 +0200

    Add estimated_size to glasses

--- sql/V1__complete.sql
+++ sql/V1__complete.sql
@@ -18,6 +18,7 @@ CREATE TABLE glasses
 (
     id integer DEFAULT nextval('glasses_id_seq'::regclass) PRIMARY KEY NOT NULL,
     name varchar(255) NOT NULL,
+    estimated_size integer,
     image_link varchar(512)
 );
 CREATE UNIQUE INDEX glasses_id_uindex ON glasses (id);
