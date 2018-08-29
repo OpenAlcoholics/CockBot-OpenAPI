8169e3b428fc7621b79fa38602a8a463e90e779a
chabare <chabare95@gmail.com>
2018-08-29 21:26:56 +0200

    Add description to ingredient_categories [skip ci]

--- sql/V1.1__Create_ingredient_categories.sql
+++ sql/V1.1__Create_ingredient_categories.sql
@@ -2,6 +2,7 @@ CREATE TABLE ingredient_categories
 (
     id integer DEFAULT nextval('ingredient_categories_id_seq'::regclass) PRIMARY KEY NOT NULL,
     name varchar(255) NOT NULL,
+    description varchar(2048),
     image_link varchar(512),
     is_accessory boolean NOT NULL,
     is_alcoholic boolean NOT NULL
