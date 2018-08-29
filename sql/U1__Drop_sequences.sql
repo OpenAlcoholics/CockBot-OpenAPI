7f105cbfc30cff88bf6e39f3311f105437c8ac48
chabare <chabare95@gmail.com>
2018-08-29 20:29:05 +0200

    Add undo versions

---
+++ sql/U1__Drop_sequences.sql
@@ -0,0 +1,6 @@
+DROP SEQUENCE IF EXISTS categories_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS drinks_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS glasses_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ingredient_categories_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ingredients_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS accessories_id_seq CASCADE;
