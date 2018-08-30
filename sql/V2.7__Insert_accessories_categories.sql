b2a2325d2a58c04f164ae3d10e9b53147d880597
chabare <chabare95@gmail.com>
2018-08-31 01:32:43 +0200

    Add missing image links

--- sql/V2.7__Insert_accessories_categories.sql
+++ sql/V2.7__Insert_accessories_categories.sql
@@ -1,11 +1,13 @@
 INSERT INTO accessories_categories (name, description, image_link)
 VALUES (
 	'Cucumber slice',
-	'Slice of a cucumber'
+	'Slice of a cucumber',
+	''
 );
 
 INSERT INTO accessories_categories (name, description, image_link)
 VALUES (
 	'Ice',
-	'This, really? It is fucking frozen water people.'
+	'This, really? It is fucking frozen water people.',
+	''
 );
