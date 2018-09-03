b4b5290bdf359e1ad4f00e8fc2497f4020789372
Chabare <Chabare@users.noreply.github.com>
2018-09-03 17:41:58 +0200

    Fix options for pk constraint

--- sql/V1.9__Create_drink_accessories.sql
+++ sql/V1.9__Create_drink_accessories.sql
@@ -6,5 +6,5 @@ CREATE TABLE drink_accessories
     before boolean, -- Should we keep this?
     CONSTRAINT drink_accessories_drinks_id_fk FOREIGN KEY (drink_id) REFERENCES drinks (id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT drink_accessories_accessories_id_fk FOREIGN KEY (accessories_id) REFERENCES accessories (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT drink_accessories_pk PRIMARY KEY (drink_id, accessories_id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT drink_accessories_pk PRIMARY KEY (drink_id, accessories_id)
 );
