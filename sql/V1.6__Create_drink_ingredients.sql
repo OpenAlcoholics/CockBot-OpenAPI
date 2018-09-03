fa541e03bcd7098f6956b45c4b5c4b1aa364b5db
Chabare <Chabare@users.noreply.github.com>
2018-09-03 17:40:28 +0200

    Fix contraint options for pk

--- sql/V1.6__Create_drink_ingredients.sql
+++ sql/V1.6__Create_drink_ingredients.sql
@@ -7,5 +7,5 @@ CREATE TABLE drink_ingredients
     CHECK (share >= 0 AND share <= 100),
     CONSTRAINT drink_ingredients_drinks_id_fk FOREIGN KEY (drink_id) REFERENCES drinks (id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT drink_ingredients_ingredients_id_fk FOREIGN KEY (ingredient_id) REFERENCES ingredients (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT drink_ingredients_pk PRIMARY KEY (drink_id, ingredient_id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT drink_ingredients_pk PRIMARY KEY (drink_id, ingredient_id)
 );
