ca915ff090c6d309e53053864251977e461f6fd9
Chabare <Chabare@users.noreply.github.com>
2018-09-10 22:38:55 +0200

    Add DB schema (#5)
    Add SQL schema
    
    Relates to #1
    
    * Add circleci and flyway migration
    
    Make a single complete.sql for flyway
    [skip ci] due to some environment variables not being in circle ci yet
    
    * Add fk for glass to drink
    
    * Remove port, should be injected
    
    * Copy sql folder into /flyway/sql
    
    * Add estimated_size to glasses
    
    * Move ingredients.measure into drink_ingredients.share
    
    * Update version
    
    * Clean db in test mode before deploying
    
    Change this if we have actual data
    
    * Split table creation into seperate files with feature version numbers
    
    * Add undo versions
    
    * Add comments
    
    * Add accessoires and joining table (drinks)
    
    Relates to #1
    
    * Add description to ingredient_categories
    [skip ci]
    
    * Preliminary commit for mock data
    
    [skip ci]
    
    * Rename insert for seperate insert files
    
    * Rename files
    
    * Update complete.sql
    
    * Add individual insert files
    
    * Add new data
    
    * Add missing semicolon
    
    * Remove preliminary strings
    
    * Add double quotes to single quote for INSERT values
    
    * Update single quote escapes
    
    * More unescapes
    
    * Remove superfluous commas
    
    * Alcohol percentage values to int
    
    * Add missing comma
    
    * Update ingredient
    
    * Add missing image links
    
    * Add composite primary key to drink_ingredients
    
    Primary key is drink_id+ingredient_id
    
    * Add composite primary key to drink_accessories
    
    Primary key is drink_id + accessories_id
    
    * Fix contraint options for pk
    
    * Fix options for pk constraint
    
    * You all know what I did here
    
    * Remove complete sql files
    
    * Finish up categories -> drink_categories
    
    * Update WHERE clauses (drink_categories)
    
    * Convert revision_date from date to integer
    
    * Update .gitignore
    
    * Add DROP command to drop_accessories
    
    * Add newline to all sql files
    
    * Move flyway.conf into .circleci folder
    
    * Add non-alcoholic drink
    
    * Replace TODOs in insert_drinks with "values"
    
    * Add locations to .circleci/flyway.conf
    
    * Remove

---
+++ sql/V2.6__Insert_drink_ingredients.sql
@@ -0,0 +1,39 @@
+INSERT INTO drink_ingredients (drink_id, ingredient_id, share, rank)
+VALUES (
+	(SELECT id FROM drinks WHERE drinks.name = 'Gin & Tonic'),
+	(SELECT id FROM ingredients WHERE ingredients.name = 'Hendricks Gin'),
+	50,
+	0
+);
+
+INSERT INTO drink_ingredients (drink_id, ingredient_id, share, rank)
+VALUES (
+	(SELECT id FROM drinks WHERE drinks.name = 'Gin & Tonic'),
+	(SELECT id FROM ingredients WHERE ingredients.name = 'Tonic Water'),
+	50,
+	0
+);
+
+INSERT INTO drink_ingredients (drink_id, ingredient_id, share, rank)
+VALUES (
+	(SELECT id FROM drinks WHERE drinks.name = 'Vodka-O'),
+	(SELECT id FROM ingredients WHERE ingredients.name = 'Orange Juice'),
+	70,
+	0
+);
+
+INSERT INTO drink_ingredients (drink_id, ingredient_id, share, rank)
+VALUES (
+	(SELECT id FROM drinks WHERE drinks.name = 'Vodka-O'),
+	(SELECT id FROM ingredients WHERE ingredients.name = 'Three Sixty Vodka'),
+	30,
+	0
+);
+
+INSERT INTO drink_ingredients (drink_id, ingredient_id, share, rank)
+VALUES (
+	(SELECT id FROM drinks WHERE drinks.name = 'Virgin Vodka-O'),
+	(SELECT id FROM ingredients WHERE ingredients.name = 'Orange Juice'),
+	100,
+	0
+);
---
+++ sql/V2.7__Insert_accessories_categories.sql
@@ -0,0 +1,13 @@
+INSERT INTO accessories_categories (name, description, image_link)
+VALUES (
+	'Cucumber',
+	'',
+	''
+);
+
+INSERT INTO accessories_categories (name, description, image_link)
+VALUES (
+	'Lemon',
+	'Slice of a cucumber',
+	''
+);
