9f73db7876fe316ef1ff8262c9f34a000788335f
chabare <chabare95@gmail.com>
2019-02-23 22:57:06 +0100

    Update pictures

--- mockSql/V2.4__Insert_drink.sql
+++ mockSql/V2.4__Insert_drink.sql
@@ -1,7 +1,7 @@
 INSERT INTO cocktail (name, image_link, description, revision_date, notes, category_id, glass_id, ice_cubes)
 VALUES (
 	'Gin & Tonic',
-	'https://www.thespruceeats.com/thmb/9g5q0ChNcHs5IjThGwciEsa5d0Y=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/gin-tonic-5a8f334b8e1b6e0036a9631d.jpg',
+	'https://images.unsplash.com/photo-1536942192778-d9aabc82a47d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9',
 	'Classic and easy, the gin and tonic is light and refreshing. It is a simple mixed drink—requiring just the two ingredients—and is perfect for happy hour, dinner, or anytime you simply want an invigorating beverage.',
 	1535500800,
 	'Hendricks Gin is best paired with a cucumber and a mold tonic water.',
@@ -13,7 +13,7 @@ VALUES (
 INSERT INTO cocktail (name, image_link, description, revision_date, notes, category_id, glass_id, ice_cubes)
 VALUES (
 	'Vodka-O',
-	'https://example.com/vodkaO.jpg',
+	'http://images.unsplash.com/photo-1542800926-98fa60157353?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9',
 	'Vodka and orange juice.',
 	1535587200,
 	'Not technically a cocktail.',
@@ -25,7 +25,7 @@ VALUES (
 INSERT INTO cocktail (name, image_link, description, revision_date, notes, category_id, glass_id, ice_cubes)
 VALUES (
 	'Virgin Vodka-O',
-	'https://example.com/o.jpg',
+	'http://images.unsplash.com/photo-1542800926-98fa60157353?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9',
 	'Vodka and orange juice, sans Vodka.',
 	1536537600,
 	'Why dont you make this one yourself?',
